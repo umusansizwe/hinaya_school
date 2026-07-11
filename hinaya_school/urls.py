d03404c8e5e6386edf98c396d348b540f5306b26
umusansizwe <umusansizwe@gmail.com>
2026-07-11 10:34:44 +0300

    Fix urls.py - add missing URLs

--- hinaya_school/urls.py
+++ hinaya_school/urls.py
@@ -15,18 +15,25 @@ urlpatterns = [
     path('headmaster/buses/', views.headmaster_buses, name='headmaster_buses'),
     path('headmaster/add_student/', views.add_student, name='add_student'),
     path('headmaster/delete_student/<int:student_id>/', views.delete_student, name='delete_student'),
-    path('headmaster/promote/', views.promote_students, name='promote_student'),
+    path('headmaster/promote/', views.promote_students, name='promote_students'),
     path('headmaster/add_teacher/', views.add_teacher, name='add_teacher'),
     path('headmaster/delete_teacher/<int:teacher_id>/', views.delete_teacher, name='delete_teacher'),
     path('headmaster/add_bus/', views.add_bus, name='add_bus'),
     path('headmaster/delete_bus/<int:bus_id>/', views.delete_bus, name='delete_bus'),
+    
+    # Accountant URLs
     path('accountant/', views.accountant_dashboard, name='accountant_dashboard'),
     path('accountant/add_payment/', views.add_payment, name='add_payment'),
+    
+    # Teacher URLs
     path('teacher/', views.teacher_dashboard, name='teacher_dashboard'),
     path('teacher/add_marks/', views.add_marks, name='add_marks'),
+    
+    # Parent URLs
     path('parent/', views.parent_dashboard, name='parent_dashboard'),
     path('parent/send_message/', views.send_message, name='send_message'),
-    path('superadmin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    
+    # Admin URLs
+    path('superadmin/', views.admin_dashboard, name='admin_dashboard'),
     path('superadmin/update_profile/', views.update_school_profile, name='update_school_profile'),
-    path('headmaster/academic_history/<int:student_id>/', views.academic_history, name='academic_history'),
 ]
